03f03eb0c33a9a727b80e9fa300f6f2182f1e774
atrim109 <atrimble1@myseneca.ca>
2022-10-02 18:48:06 -0400

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+var path = require("path");
+//require("data-service.js");
+app.use(express.static('public'));
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    console.log("Express http server listening on " + HTTP_PORT);
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
