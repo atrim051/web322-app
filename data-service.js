2d76ed8e60e47e578fe69f5b9527719e8ebdbd91
atrim109 <atrim051@gmail.com>
2022-10-19 22:36:46 -0400

    new

--- data-service.js
+++ data-service.js
@@ -31,44 +31,44 @@ module.exports.getAllEmployees = function () {
   });
 };
 
-module.exports.getManagers = function () {
-  return new Promise((resolve, reject) => {
-    var managers = [];
-    for (let i = 0; i < employees.length; i++) {
-      if (employees[i].isManager == true) {
-        managers.push(employees[i]);
-      }
-    }
-    if (managers.length == 0) {
-      reject('No Results Returned');
-    }
-    resolve(employees);
-  });
-};
-
-module.exports.getDepartments = function () {
-  return new Promise((resolve, reject) => {
-    if (departments.length == 0) {
-      reject('No Results Returned');
-    }
-    resolve(departments);
-  });
-};
-
 module.exports.addEmployee = function (employeeData) {
   return new Promise((resolve, reject) => {
     if (employeeData.isManager == undefined) 
-      employeeData.isManager = false;
+    employeeData.isManager = false;
     else 
-      employeeData.isManager = true;
+    employeeData.isManager = true;
     employeeData.employeeNum = employees.length + 1;
     employees.push(employeeData);
     resolve(
       `Employee #${employeeData.employeeNum} has been added successfully!`
-    );
-  });
-};
-
+      );
+    });
+  };
+  
+  module.exports.getDepartments = function () {
+    return new Promise((resolve, reject) => {
+      if (departments.length == 0) {
+        reject('No Results Returned');
+      }
+      resolve(departments);
+    });
+  };
+  
+  module.exports.getManagers = function () {
+    return new Promise((resolve, reject) => {
+      var managers = [];
+      for (let i = 0; i < employees.length; i++) {
+        if (employees[i].isManager === true) {
+          managers.push(employees[i]);
+        }
+      }
+      if (managers.length == 0) {
+        reject('No Results Returned');
+      }
+      resolve(employees);
+    });
+  };
+  
 module.exports.getEmployeesByStatus = (status) => {
   return new Promise((resolve, reject) => {
     var empByStatus = [];
